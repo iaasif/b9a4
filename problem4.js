6356ff924c1fbe2eda61026ec063cb154d30fdb3
iaasif <imtiazasif.cse@gmail.com>
2024-02-08 02:54:25 +0600

    problem 4 solved

--- problem4.js
+++ problem4.js
@@ -2,27 +2,24 @@
 // 1st 
 function password(obj) {
     //write your code here
+    // if(typeof obj.name == 'string' && obj.birthYear.toString().length == 4 && typeof siteName == 'string'){
 
-    console.log(obj)
-    // Google#kolimuddin@1999
-    let str = obj.siteName + "#" + obj.name + "@" + obj.birthYear;
-    console.log(str);
-    // let newStr = str.charAt(0).toUpperCase();
-    // console.log(new);
-    newStr = str.charAt(0).toUpperCase() + str.slice(1);
 
-    console.log(newStr);
+    // }
 
+    if ('name' in obj && 'birthYear' in obj && obj.birthYear.toString().length == 4 && 'siteName' in obj) {
+
+        // console.log(obj)
+        // Google#kolimuddin@1999
+        let str = obj.siteName + "#" + obj.name + "@" + obj.birthYear;
+        // console.log(str);
+        // let newStr = str.charAt(0).toUpperCase();
+        // console.log(new);
+        newStr = str.charAt(0).toUpperCase() + str.slice(1);
+
+        return newStr;
+    }
+    else
+        // console.log("invalid");
+        return "invalid";
 };
-
-
-
-let object = {
-    name: "kolimuddin",
-    birthYear: 1999,
-    siteName: "google",
-    bal: true,
-}
-
-
-password(object);
