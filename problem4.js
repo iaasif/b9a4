c14acc6a359c8347b4fe6bbd60f79e10615b049e
iaasif <imtiazasif.cse@gmail.com>
2024-02-08 02:38:21 +0600

    almost solve every programm  only 4 no error handeling unfinished

--- problem4.js
+++ problem4.js
@@ -1,25 +1,28 @@
 /*function signature/sample */
 // 1st 
-function password({ name, birthYear, siteName }) {
-    //write your code here
-
-    console.log(name, birthYear, siteName)
-};
-
-// 2nd 
 function password(obj) {
     //write your code here
 
-    console.log(name, birthYear, siteName)
+    console.log(obj)
+    // Google#kolimuddin@1999
+    let str = obj.siteName + "#" + obj.name + "@" + obj.birthYear;
+    console.log(str);
+    // let newStr = str.charAt(0).toUpperCase();
+    // console.log(new);
+    newStr = str.charAt(0).toUpperCase() + str.slice(1);
+
+    console.log(newStr);
+
 };
 
 
-let obj = {
+
+let object = {
     name: "kolimuddin",
     birthYear: 1999,
-    siteName: "google ",
+    siteName: "google",
     bal: true,
 }
 
 
-password(obj);
+password(object);
